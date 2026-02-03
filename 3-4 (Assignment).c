1acf49b9282577b3bd1241fda15d9ddb4e1c526b
Dhwanit Mehta <76042300+nit-codes-like-alot@users.noreply.github.com>
2026-02-03 16:02:30 +0530

    Update 3-4 (Assignment) student array of pointers.c

--- 3-4 (Assignment).c
+++ 3-4 (Assignment).c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    char names[n][50];
+
+    printf("Enter student names:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%s", names[i]);
+    }
+
+    printf("\nStudent Names:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", names[i]);
+    }
+
+    return 0;
+}
