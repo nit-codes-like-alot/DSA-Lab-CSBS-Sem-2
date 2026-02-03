176c90e65bfb3aaa39175df0d6a44fe9b2d1f017
Dhwanit Mehta <76042300+nit-codes-like-alot@users.noreply.github.com>
2026-02-03 15:38:59 +0530

    Update 3-3- swap using pt n fn.c

--- 3-3-.c
+++ 3-3-.c
@@ -1,1 +1,23 @@
-// adding later
+#include<stdio.h>
+void swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+int main(){
+    int x,y;
+    int a[]={1,2,3,4,5,6};
+    printf("Enter the positions to swap");
+    printf("\nindex 1: ");
+    scanf("%d",&x);
+    printf("\nindex 2: ");
+    scanf("%d",&y);
+    int *c=&a[x-1];
+    int *d=&a[y-1];
+    swap(c,d);
+    printf("New array is: ");
+    for(int i=0;i<6;i++){
+        printf("%d,",a[i]);
+    }
+}
