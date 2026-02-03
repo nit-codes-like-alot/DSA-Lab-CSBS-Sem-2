7928d5485eae26926963f50f5c8da77dd9b312e5
Dhwanit Mehta <76042300+nit-codes-like-alot@users.noreply.github.com>
2026-02-03 15:29:28 +0530

    Update 1-4 - enter and delete el from specific position type shi.c

--- 1-4 - enter and delete el from specific position type shi.c
+++ 1-4 - enter and delete el from specific position type shi.c
@@ -1,1 +1,30 @@
-//ill add the code later
+#include<stdio.h>
+void main(){
+    int s,n,key;
+    
+    printf("Enter the size of array: ");
+    scanf("%d",&s);
+    int a[s];
+    
+    printf("Enter the position at which new elementis to be inserted: ");
+    scanf("%d",&n);
+    
+    printf("Enter the new element: ");
+    scanf("%d",&key);
+    
+    //taking inputs of elements
+    for(int i=0;i<s;i++){
+        printf("Enter element %d: ",i+1);
+        scanf("%d",&a[i]);
+    }
+    //entering the new element atspecified postion
+    for(int i=0;i<s;i++){
+        if((i+1)==n){
+            a[i]=key;
+        }
+    }
+    //printing the modified array
+    for(int i=0;i<s;i++){
+        printf("%d\t",a[i]);
+    }
+}
